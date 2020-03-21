b1b871f1adccaff7bcbc5c745ef05d4dd4d29439
Randy Wressell <randy5235@gmail.com>
2020-03-20 21:37:16 -0700

    Add npm module for exif

--- ui/src/components/UploadForm.js
+++ ui/src/components/UploadForm.js
@@ -1,92 +1,75 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import EXIF from "exif-js";
 
-class UploadForm extends React.Component{
-
-  /*global componentDidMount EXIF:true*/ // stop eslint from throwing errors/warnings since ol depends on the following external script
-  componentDidMount(){
-    const script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/npm/exif-js"; // For retrieving Exif data
-    script.async = true;
-    document.head.appendChild(script);
-  }
-
-  ValidateForm(event)
-  {
+class UploadForm extends React.Component {
+  ValidateForm(event) {
     // Convert gps coordinates as presented in exif data to a comma separated decimal pair.
-    function GPSDegToDecString( lat, latRef, lon, lonRef)
-    {
-
+    function GPSDegToDecString(lat, latRef, lon, lonRef) {
       var retLat = 0;
-      var latComps = lat.toString().split(',');
+      var latComps = lat.toString().split(",");
       retLat = parseInt(latComps[0]);
-      retLat += parseInt(latComps[1])/60;
-      retLat += parseInt(latComps[2])/3600;
-      if(latRef === 'S')
-      {
-        latRef = 0-latRef;
+      retLat += parseInt(latComps[1]) / 60;
+      retLat += parseInt(latComps[2]) / 3600;
+      if (latRef === "S") {
+        latRef = 0 - latRef;
       }
 
       var retLon = 0;
-      var lonComps = lon.toString().split(',');
+      var lonComps = lon.toString().split(",");
       retLon = parseInt(lonComps[0]);
-      retLon += parseInt(lonComps[1])/60;
-      retLon += parseInt(lonComps[2])/3600;
-      if(latRef === 'W')
-      {
-        latRef = 0-latRef;
+      retLon += parseInt(lonComps[1]) / 60;
+      retLon += parseInt(lonComps[2]) / 3600;
+      if (latRef === "W") {
+        latRef = 0 - latRef;
       }
-      console.log(lat,' ', latRef, ' ', lon, ' ', lonRef);
-      console.log(retLat+" "+retLon);
-      return retLat+","+retLon;
-
+      console.log(lat, " ", latRef, " ", lon, " ", lonRef);
+      console.log(retLat + " " + retLon);
+      return retLat + "," + retLon;
     }
 
-      event.preventDefault();
-      var uploadForm = document.getElementsByClassName('upload-file')[0];
+    event.preventDefault();
+    var uploadForm = document.getElementsByClassName("upload-file")[0];
 
-      var files = document.getElementById('upload-image').files;
-      var locInput = document.getElementById('upload-location');
+    var files = document.getElementById("upload-image").files;
+    var locInput = document.getElementById("upload-location");
 
-      if (files[0] !== undefined) // file selected
-      {
-        if (files[0].size > 8*1024*1024) // 8 MB size limit
-        {
-          console.log('file too large');
-          return false;
-        }
-        else if  (locInput.value.length > 0) // location manually entered
-        {
-          // should check for formatting here
-          uploadForm.submit();
-          return true;
-        }
-        else
-        {
-          EXIF.getData(files[0], function()
-        {
+    if (files[0] !== undefined) {
+      // file selected
+      if (files[0].size > 8 * 1024 * 1024) {
+        // 8 MB size limit
+        console.log("file too large");
+        return false;
+      } else if (locInput.value.length > 0) {
+        // location manually entered
+        // should check for formatting here
+        uploadForm.submit();
+        return true;
+      } else {
+        EXIF.getData(files[0], function() {
           var lat = EXIF.getTag(this, "GPSLatitude");
           var latRef = EXIF.getTag(this, "GPSLatitudeRef");
           var lon = EXIF.getTag(this, "GPSLongitude");
           var lonRef = EXIF.getTag(this, "GPSLongitudeRef");
 
           // if any gps data is bad or nonexistent, request it manually
-          if (lat === undefined || latRef === undefined || lon === undefined || lonRef === undefined)
-          {
+          if (
+            lat === undefined ||
+            latRef === undefined ||
+            lon === undefined ||
+            lonRef === undefined
+          ) {
             locInput.type = "text";
             return false;
-          }
-          else // set the location input (Should confirm the formatting)
-          {
+          } // set the location input (Should confirm the formatting)
+          else {
             locInput.value = GPSDegToDecString(lat, latRef, lon, lonRef);
             uploadForm.submit();
             return true;
           }
         });
       }
-    }
-    else
-    {
-      console.log('file not defined');
+    } else {
+      console.log("file not defined");
       return false;
     }
 
@@ -95,19 +78,26 @@ class UploadForm extends React.Component{
 
   render() {
     return (
-      <form className="upload-file"
-      action="http://localhost:3000/api/upload"
-      method="post"
-      encType="multipart/form-data"
-      onSubmit={this.ValidateForm}>
+      <form
+        className="upload-file"
+        action="http://localhost:3000/api/upload"
+        method="post"
+        encType="multipart/form-data"
+        onSubmit={this.ValidateForm}
+      >
         <div>
           <input id="upload-image" name="image" type="file" accept="image/*" />
-          <input id= "upload-location" name="location" type="hidden" placeholder="no exif, gps coordinates please"/>
+          <input
+            id="upload-location"
+            name="location"
+            type="hidden"
+            placeholder="no exif, gps coordinates please"
+          />
           <input type="submit" value="Upload" />
         </div>
       </form>
     );
   }
-};
+}
 
 export default UploadForm;
