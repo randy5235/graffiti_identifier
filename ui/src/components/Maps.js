91617720a77ed57f9cd78b44c5c9bc0fdd75ef24
El1-G <56484497+El1-G@users.noreply.github.com>
2020-03-22 13:51:34 -0700

    Replace leaflet cdn with npm module, replace opencage with leaflet-control geocoder (temporarily using nominatim)

--- ui/src/components/Maps.js
+++ ui/src/components/Maps.js
@@ -1,5 +1,6 @@
 import React from "react";
 import L from "leaflet";
+import LCG from 'leaflet-control-geocoder';
 import "../styles/Leaflet.css";
 import "../styles/OpenCage.css";
 import iconImg from "../styles/images/marker-icon.png";
@@ -9,78 +10,96 @@ class MapsPage extends React.Component {
   constructor(props) {
     super(props);
     this.myRef = React.createRef;
-  } /*eslint no-undef: "warn"*/ // stop eslint from throwing errors/warnings since ol depends on the following external script
+  }
 
-  /*global componentDidMount L, map, ol:true*/ componentDidMount() {
-    // const leafletScript = document.createElement("script");
-    const openCageScript = document.createElement("script");
-    // leafletScript.async = true;
-    openCageScript.async = true;
-    // leafletScript.src = "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js";
-    openCageScript.src =
-      "https://cdn.jsdelivr.net/gh/opencagedata/leaflet-opencage-search@1.3.0/dist/js/L.Control.OpenCageSearch.min.js";
+  /*global componentDidMount L, map:true*/ 
+  componentDidMount() {
 
-    //document.head.appendChild(leafletScript);
-    //leafletScript.onload = function() {
-    document.head.appendChild(openCageScript);
-    //};
-    var lat = 37.41;
-    var lon = 8.82;
-    const error = error => console.log(error);
+    //default coordinates to roughly Los Angeles
+    var lat = 34;
+    var lon = -118;
+    var map = {};
+    var mapZoom = 8;
+
+    // render default on browser location denial
+    const error = error => {
+      renderMap();
+      console.log(`ERROR: `,error);
+    };
+
+    // Acquire browser location
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
+        // render map to browser location
         function(position) {
           lat = position.coords.latitude;
           lon = position.coords.longitude;
-          console.log(lat, lon);
+          mapZoom = 13;
+          renderMap();
         },
         error,
         { enableHighAccuracy: true }
       );
     }
-    openCageScript.onload = function() {
-      // onload not supported by older IE TODO: add readystate functionality
-      // =================
-      // Map functionality
-      // =================
+
+    //Load us up a map
+    function renderMap() {
+
       var mapOptions = {
         center: [lat, lon],
-        zoom: 20
+        zoom: mapZoom
       };
-      const openCageKey = process.env.REACT_APP_OPENCAGE_API_KEY;
-      console.log(`key value`, openCageKey);
-      var geocodeOptions = {
-        key: "bf942964c7504d93987e3decca324bc6", // Testing level api key, 2500 requests per day, prefferably less than 1 per second.
-        limit: 10,
-        proximity: lat + ", " + lon
-      };
+        
+        map =  new L.map("map", mapOptions);
+        
+        var layer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+          
+        map.addLayer(layer);
+          
+        // define a custom marker icon as leaflet's default image pathing is broken...
+         var markerIcon = L.icon({
+          iconUrl: iconImg,
+          shadowUrl: iconShadow,
+            
+          iconSize: [20, 30], // size of the icon
+          shadowSize: [20, 30], // size of the shadow
+          iconAnchor: [10, 0], // point of the icon which will correspond to marker's location
+          shadowAnchor: [5, 0], // the same for the shadow
+          popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        }); 
+          
+        // template for adding a marker
 
-      var map = new L.map("map", mapOptions);
-
-      var layer = new L.TileLayer(
-        "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      );
-
-      map.addLayer(layer);
-
-      var markerIcon = L.icon({
-        iconUrl: iconImg,
-        shadowUrl: iconShadow,
-
-        iconSize: [20, 30], // size of the icon
-        shadowSize: [20, 30], // size of the shadow
-        iconAnchor: [10, 0], // point of the icon which will correspond to marker's location
-        shadowAnchor: [5, 0], // the same for the shadow
-        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-      });
-
-      var marker = L.marker([lat, lon], { icon: markerIcon });
-
-      marker.addTo(map);
-
-      var control = L.Control.openCageSearch(geocodeOptions).addTo(map);
-    };
-  }
+        //var marker = L.marker([lat, lon], {icon: markerIcon});
+        //marker.addTo(map);
+        
+        // Search for location information based on Map click and add a popup marker
+        const control = L.Control.GeoCoder
+            
+          const geocoder = L.Control.Geocoder.nominatim();
+          let marker;
+            
+            map.on('click', e => {
+              geocoder.reverse(e.latlng, map.options.crs.scale(map.getZoom()), results => {
+                var r = results[0];
+                if (r) {
+                  if (marker) {
+                    marker.
+                    setLatLng(r.center).
+                    setPopupContent(r.html || r.name).
+                    openPopup();
+                  } else {
+                    marker = L.marker(r.center)
+                    .bindPopup(r.name)
+                    .addTo(map)
+                    .openPopup();
+                  }
+                }
+              })
+            })
+            
+          }
+          }
 
   render() {
     return (
